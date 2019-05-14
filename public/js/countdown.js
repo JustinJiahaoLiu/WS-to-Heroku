c437fae4ba45dcabf2f5703bd30dbc915bf3c2c0
Justin Liu <justinjiahaoliu@gmail.com>
2019-05-14 20:33:15 +1000

    Refactor countdown animation to function

--- public/js/countdown.js
+++ public/js/countdown.js
@@ -1,10 +1,34 @@
-var countdownNumberEl = document.getElementById('countdown-number');
+var countdownNumberEl = document.createElement('div');
+countdownNumberEl.setAttribute('id', 'countdown-number');
 var countdown = 20;
+var countdownBox = document.getElementById('countdown');
+var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+var circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+circle.setAttribute("r", "18");
+circle.setAttribute("cx", "20");
+circle.setAttribute("cy", "20");
 
-countdownNumberEl.textContent = countdown;
+svg.appendChild(circle);
 
-setInterval(function() {
-  countdown = --countdown <= 0 ? 20 : countdown;
 
-  countdownNumberEl.textContent = countdown;
-}, 1000);
+var timer;
+
+function gameCountdownON(){
+	countdownNumberEl.innerHTML = countdown;
+	countdownBox.appendChild(countdownNumberEl);
+	countdownBox.appendChild(svg);
+	timer = setInterval(function() {
+  				countdown = --countdown <= 0 ? 20 : countdown;
+
+ 		 		countdownNumberEl.textContent = countdown;
+			}, 1000);
+}
+
+function gameCountdownOFF(){
+	//clear number and circle
+	while (countdownBox.firstChild) {
+    	countdownBox.removeChild(countdownBox.firstChild);
+	}
+	countdown = 20; //reset countdown
+	clearInterval(timer);	//reset countdown timer
+}
