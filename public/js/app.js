67e0021f3b8fc4ebb8e52ff3ab8f36d6bc549281
W126 <W126@SYDRES.SydneyTAFE.edu.au>
2019-05-16 14:44:00 +1000

    Split the overlay into a different page...

--- public/js/app.js
+++ public/js/app.js
@@ -1,99 +1,87 @@
+var urlParams = new URLSearchParams(window.location.search);
 var HOST = location.origin.replace(/^http/, 'ws');
-var name;
-var socket;
+var name = urlParams.get('clientName');
+var socket = new WebSocket(HOST);;
 var game_state;
 
 
-function openSocket(){
-    name = document.getElementById('inputName').value;
-    if(name==""){
-        document.getElementById('nameError').style.display = "";
-        return;
-    }else{
-        document.getElementById('overlay').style.display = "none";
-        socket = new WebSocket(HOST);    //Open socket
-
-        //Send client name to srever
-        socket.onopen = (event) => {
-            console.log('Socket Open!');
-            socket.send(JSON.stringify({
-                type:'name',
-                data: name
-            }));
-        };
+ //Send client name to srever
+socket.onopen = (event) => {
+    console.log('Socket Open!');
+    socket.send(JSON.stringify({
+        type:'name',
+        data: name
+    }));
+};
 
 
-        socket.onmessage = (event) =>{
-            console.log(event);
-            var json = JSON.parse(event.data);
-            //console.log(json);
+socket.onmessage = (event) =>{
+    console.log(event);
+    var json = JSON.parse(event.data);
+    //console.log(json);
 
-            /*------------Game Mode------------------*/
-            if(json.type == "game"){
+    /*------------Game Mode------------------*/
+    if(json.type == "game"){
 
-                if(json.id != 999 && !(json.id % 100)){    //if id = 100, 200, 300, 400...
-                    //Disable game button!!
-                    document.querySelector("#gameBox").style.display = "none";
-                    //Set message to game mode
-                    document.querySelector("#messageBtn").setAttribute('onclick','gameMsg()');
-                    //check the current steps of the game status
+        if(json.id != 999 && !(json.id % 100)){    //if id = 100, 200, 300, 400...
+            //Disable game button!!
+            document.querySelector("#gameBox").style.display = "none";
+            //Set message to game mode
+            document.querySelector("#messageBtn").setAttribute('onclick','gameMsg()');
+            //check the current steps of the game status
 
-                    //show the questions once its from server
-                    let elem = document.createElement("h3");
-                    elem.setAttribute("class", "game");
-                    elem.innerHTML = json.data;
-                    document.querySelector(".container").appendChild(elem);
-                }else if(!((json.id - 1) % 100)){    //game intervel
-                    console.log("We are in");
-                    game_state = json.id;
-                    //Disable game button!!
-                    document.querySelector("#gameBox").style.display = "none";
-                    //Set message to message mode
-                    gameExit();
-                    //Show next button!!
-                    document.querySelector("#gameBoxNext").style.display = "";
+            //show the questions once its from server
+            let elem = document.createElement("h3");
+            elem.setAttribute("class", "game");
+            elem.innerHTML = json.data;
+            document.querySelector(".container").appendChild(elem);
+        }else if(!((json.id - 1) % 100)){    //game intervel
+            console.log("We are in");
+            game_state = json.id;
+            //Disable game button!!
+            document.querySelector("#gameBox").style.display = "none";
+            //Set message to message mode
+            gameExit();
+            //Show next button!!
+            document.querySelector("#gameBoxNext").style.display = "";
 
-                }else{
-                    return; //new user joins under message mode
-                }
-
-            }else if(json.type == "gameExit"){
-
-                gameExit();  //Turn off game mode
-
-            }else{
-            /*------------Message Mode------------------*/
-                //Game announcement
-                if(json.type == "message" && json.name == "announcement"){
-                    let elem = document.createElement("h3");
-                    elem.setAttribute("class", "game");
-                    elem.innerHTML = "&#127881;" + json.data + "&#127881;";
-                    document.querySelector(".container").appendChild(elem);
-                    autoScrollBottom();    //Autoscroll
-                    return;
-                }
-                //when client left
-                if(json.type == "message" && json.data == "left"){
-                    let elem = document.createElement("p");
-                    elem.setAttribute("class", "server");
-                    elem.innerHTML = json.name + " has left";
-                    document.querySelector(".container").appendChild(elem);
-                }else{
-                    //normal message
-                    var elem = document.createElement("li");
-                    elem.setAttribute("class", "others");
-                    elem.innerHTML = json.name + ": " + json.data;
-                    document.querySelector(".container").appendChild(elem);
-                }
-            }
-
-            //scroll chat box to bottom
-            autoScrollBottom();
+        }else{
+            return; //new user joins under message mode
         }
 
-    }
-}
+    }else if(json.type == "gameExit"){
 
+        gameExit();  //Turn off game mode
+
+    }else{
+    /*------------Message Mode------------------*/
+        //Game announcement
+        if(json.type == "message" && json.name == "announcement"){
+            let elem = document.createElement("h3");
+            elem.setAttribute("class", "game");
+            elem.innerHTML = "&#127881;" + json.data + "&#127881;";
+            document.querySelector(".container").appendChild(elem);
+            autoScrollBottom();    //Autoscroll
+            return;
+        }
+        //when client left
+        if(json.type == "message" && json.data == "left"){
+            let elem = document.createElement("p");
+            elem.setAttribute("class", "server");
+            elem.innerHTML = json.name + " has left";
+            document.querySelector(".container").appendChild(elem);
+        }else{
+            //normal message
+            var elem = document.createElement("li");
+            elem.setAttribute("class", "others");
+            elem.innerHTML = json.name + ": " + json.data;
+            document.querySelector(".container").appendChild(elem);
+        }
+    }
+
+    //scroll chat box to bottom
+    autoScrollBottom();
+}
 
 function sendMsg(){
 
